65d446198bc301897dd37877f413b6b12f2094a9
KonstantinTarutko <116430353+KonstantinTarutko@users.noreply.github.com>
2023-02-07 23:08:18 +0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -8,7 +8,52 @@
 Результат через список: Осень
 Результат через словарь: Осень"""
 
+"""первый вариант"""
 month = int(input("Введите месяц в виде целого числа: "))
+
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+for i in list_1:
+    if month == 1:
+        print("Зима")
+    elif i in list_1:
+        if month == 2:
+            print("Зима")
+        elif i in list_1:
+            if month == 3:
+                print("Весна")
+            elif i in list_1:
+                if month == 4:
+                    print("Весна")
+                elif i in list_1:
+                    if month == 5:
+                        print("Весна")
+                    elif i in list_1:
+                        if month == 6:
+                            print("Лето")
+                        elif i in list_1:
+                            if month == 7:
+                                print("Лето")
+                            elif i in list_1:
+                                if month == 8:
+                                    print("Лето")
+                                elif i in list_1:
+                                    if month == 9:
+                                        print("Осень")
+                                    elif i in list_1:
+                                        if month == 10:
+                                            print("Осень")
+                                        elif i in list_1:
+                                            if month == 11:
+                                                print("Осень")
+                                            elif i in list_1:
+                                                if month == 12:
+                                                    print("Зима")
+                                                    
+ 
+
+"""ниже второй вариант, но сравнение через & не получилось, к сожалению, реализовать"""
+"""month = int(input("Введите месяц в виде целого числа: "))
 
 list_1 = [1, 2, 3]
 list_2 = [4, 5, 6]
@@ -33,4 +78,4 @@ elif month > 9:
         print("Осень")
 
 else:
-        print("Введено некорректное число")
+        print("Введено некорректное число")"""
